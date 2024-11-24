712248ccfb68f0214b6646e5886bbf4dd84de8eb
surajyadav <surajyadav020406@gmail.com>
2024-11-25 02:11:27 +0530

    Setting Up Notification-Manager

---
+++ src/test/java/com/bridge/skill/notificationmanager/NotificationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridge.skill.notificationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
